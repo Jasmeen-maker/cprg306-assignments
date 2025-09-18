93d4f72bee760c9c35cb9aae7a9ae96cc9f7aea1
Jasmeen-maker <Jasmeen.Kaur3@edu.sait.ca>
2025-09-18 13:51:27 -0400

    commit

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href = "C:\cprg306\cprg306-assignments\app\week-2">Go to Week-2</Link>
+      <Link href = "\week-2">Go to Week 2</Link>
     </main>
   );
 }
