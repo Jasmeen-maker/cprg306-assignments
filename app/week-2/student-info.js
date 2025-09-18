f400ba8919f8622669419b6f3887fd1e56b75cb4
Jasmeen-maker <Jasmeen.Kaur3@edu.sait.ca>
2025-09-18 13:45:18 -0400

    commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Jasmeen</h1>
+      <Link href="https://github.com/Jasmeen-maker">https://github.com/Jasmeen-maker</Link>
+    </main>
+  );
+}
