d7ec26ac0ae9b06c029281a273f409a40c9cacd9
jasmeen kaur <jasmeen.kaur3@edu.sait.ca>
2025-09-25 14:53:46 -0400

    commit

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,9 +1,9 @@
-import ItemList from "./ItemList";
+import ItemList from "./item-list";
 
 export default function Page() {
   return (
-    <main className="min-h-screen bg-gray-50 flex flex-col items-center justify-start p-6">
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">Shopping List</h1>
+    <main className="p-6 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold mb-6 text-center">Shopping List 🛒</h1>
       <ItemList />
     </main>
   );
