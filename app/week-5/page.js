f1da3a68a8322955390ddde365ca487c3dc3c143
jasmeen kaur <jasmeen.kaur3@edu.sait.ca>
2025-10-14 13:12:21 -0400

    commit

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main className="min-h-screen bg-gray-100 p-8">
       <h1 className="text-3xl font-bold text-center mb-8 text-gray-700">
-        Week 5 - Add Item
+        Week 5 - New Item
       </h1>
       <NewItem />
     </main>
