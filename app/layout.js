cbea9f77648f28594860efa7b48faba840b80bb3
jasmeen kaur <jasmeen.kaur3@edu.sait.ca>
2025-11-19 16:01:32 -0500

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AuthContextProvider } from "./contexts/AuthContext.js";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,10 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {/* Wrap everything with AuthContextProvider */}
-        <AuthContextProvider>
-          {children}
-        </AuthContextProvider>
+        {children}
       </body>
     </html>
   );
